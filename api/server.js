47527ea9d33b3d1def681952805ea36069ac0f8d
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-24 11:13:26 +0000

    feature: Add base welcome get route

--- api/server.js
+++ api/server.js
@@ -6,6 +6,16 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors);
+server.use(cors());
+
+server.get('/', (req, res, next) => {
+  try {
+    res
+      .status(200)
+      .send(`<h3>Welcome to jwt user service</h3>`);
+  } catch (error) {
+      next(new Error('Server not available'));
+  }
+});
 
 module.exports = server;
