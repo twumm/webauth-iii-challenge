b129acfe849f4e74ff1ba7e4eb14b78b38eb50f4
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-24 14:23:04 +0000

    feature: Add method to sign-in, sign-up, get all users from server Pass these methods as props to the various components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 
@@ -7,7 +7,50 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import ListUsers from './components/ListUsers';
 
+const authUrl = 'http://localhost:5000'
+
 function App() {
+  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [requestError, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const signUp = async (userObject) => {
+    setLoading(true);
+    try {
+      const newUser = await axios.post(`${authUrl}/api/register`, userObject);
+      setUser(newUser.data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signIn = async (userObject) => {
+    setLoading(true);
+    try {
+      const user = await axios.post(`${authUrl}/api/login`, userObject);
+      await localStorage.setItem('token', user.data.token);
+      setUser(user.data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const users = await axios.get(`${authUrl}/api/users`);
+      setUsers(users);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <Router>
       <div className="App">
@@ -40,24 +83,34 @@ function App() {
 
         <Route
           path="/"
-          exact
           render={props => (
             <ListUsers
               {...props}
+              users={users}
+              error={requestError}
+              loading={loading}
             />
           )}
         />
 
         <Route
           path="/sign-in"
-          exact
-          component={SignIn}
+          render={props => (
+            <SignIn
+              {...props}
+              signIn={signIn}
+            />
+          )}
         />
 
         <Route
           path="/sign-up"
-          exact
-          component={SignUp}
+          render={props => (
+            <SignUp
+              {...props}
+              signUp={signUp}
+            />
+          )}
         />
 
       </div>
