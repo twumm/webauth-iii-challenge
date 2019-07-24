f52749a59ffed9b698bfb52a53e4255a9669cc9f
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-24 11:47:03 +0000

    feature: Create CRUD functions for users table

---
+++ api/users/users-model.js
@@ -0,0 +1,44 @@
+const db = require('../database/dbConfig');
+
+module.exports = {
+  getAllUsers,
+  findBy,
+  findById,
+  addUser,
+  updateUser,
+  removeUser
+};
+
+function getAllUsers() {
+  return db('users')
+    .select('id', 'username', 'department');
+}
+
+function findBy(filter) {
+  return db('users')
+    .where(filter)
+    .select('id', 'username', 'department');
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+
+async function addUser(user) {
+  const [ id ] = await db('users').insert(user);
+  return findById(id);
+}
+
+function updateUser(id, changes) {
+  return db('users')
+    .where({ id })
+    .update(changes);
+}
+
+function removeUser(id) {
+  return db('users')
+    .where({ id })
+    .del();
+}
